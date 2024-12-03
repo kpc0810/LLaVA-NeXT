0b4bdf67a3bc404fd4cdaae6018289a871a59a4f
kpc0810 <irving870810@gmail.com>
2024-12-03 02:04:57 -0800

    [add] forgetting to push builder.py of cp

---
+++ llava/model/contrastive_projector/builder.py
@@ -0,0 +1,15 @@
+import os
+import torch
+
+from .base_projector import ContrastiveProjectorConfig, ContrastiveProjector
+from transformers import PretrainedConfig, PreTrainedModel
+
+
+def build_contrastive_projector_config(config: PretrainedConfig):
+    return 
+
+def build_contrastive_projector(
+    config: PretrainedConfig, modality: str
+) -> PreTrainedModel:
+    cp_config = ContrastiveProjectorConfig(config)
+    return ContrastiveProjector(cp_config)
