e4a7bc7fd7857c6649b5072155e949bea673a16c
kpc0810 <irving870810@gmail.com>
2024-12-26 23:08:01 -0800

    [feat.] pred & eval on dream1k

--- scripts/pred/dream1k/interact_single_ckpt.sh
+++ scripts/pred/dream1k/interact_single_ckpt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source activate llava
+source /lustre/fsw/portfolios/nvr/users/${USER}/miniconda3/bin/activate llava-eval
 which python
 
 master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
@@ -8,23 +8,23 @@ export CURRENT_RANK=${SLURM_PROCID:-"0"}
 export HYDRA_FULL_ERROR="1"
 export DECORD_EOF_RETRY_MAX="100000"
 worker_list=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | tr '\n' ' ')
-n_node=${SLURM_JOB_NUM_NODES:-1}
-# n_node=1
 
-cd /mnt/home/kaipoc/research_vh/LLaVA-NeXT
+cd /home/kaipoc/personal/research_vh/LLaVA-NeXT/
 
-nproc_per_node=2
-data_path="DREAM-1K/json/metadata.json"
-video_folder="DREAM-1K/video/DREAM-1K_videos"
-output_dir="outputs/dream1k"
-output_name="dream1k_pred_results_"
+n_node=1
+nproc_per_node=1
+data_path="playground/DREAM-1K/json/metadata.json"
+video_folder="playground/DREAM-1K/video/DREAM-1K_videos"
+output_dir="outputs/dream1k/pred_results/__debug"
+output_name="__debug"
+model_path="checkpoints/exps/llava-qwen-7b_fcl-ehn_nnode=32_epo=2_plmlr=1e-5_vtlr=5e-6_bspd=1_cplr=1e-4_aslr=1e-4_vcclwt=0.5_tpoclwt=0.25_tpaclwt=0.25/checkpoint-1000"
 question="Please provide a detailed description of the video, focusing on the main subjects, their actions, and the background scenes."
 
 # CUDA_VISIBLE_DEVICES=0 torchrun llava/eval/dream1k.py \
 torchrun --nnodes="${n_node}" --nproc_per_node="${nproc_per_node}" --master_port=51466 \
     --master_addr "${MASTER_ADDR}" --node_rank="${CURRENT_RANK}" \
-    llava/eval/dream1k.py \
-    --model-path "lmms-lab/LLaVA-Video-7B-Qwen2" \
+    llava/eval/model_caption_dream1k.py \
+    --model-path "${model_path}" \
     --data-file "${data_path}" \
     --video_folder "${video_folder}" \
     --output-dir "${output_dir}" \
